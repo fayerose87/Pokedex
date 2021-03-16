30219eea4f0c353ac10ebec4369c9b85e465a35f
fayerose87 <79806977+fayerose87@users.noreply.github.com>
2021-03-15 21:31:57 -0500

    Created variable pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,17 @@
-alert('Hello world');
-
-let favoriteFood = 'chocolate chip cookies';
-document.write(favoriteFood);
+let pokemonList = [
+  {
+    name: 'Articuno',
+    height: 1.7,
+    type: ['ice','flying']
+    },
+  {
+    name: 'Zapdos',
+    height: 1.6,
+    type: ['electric','flying']
+    },
+  {
+    name: 'Moltres',
+    height: 2,
+    type: ['fire','flying']
+  }
+];
