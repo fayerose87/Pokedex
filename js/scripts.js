a91c34a125e456f2c772470573a6df8ad2f198c3
fayerose87 <79806977+fayerose87@users.noreply.github.com>
2021-03-17 21:48:23 -0500

    Created for loop for pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,27 @@
 let pokemonList = [
   {
     name: 'Articuno',
-    height: 1.7,
+    height: 3,
     type: ['ice','flying']
     },
   {
     name: 'Zapdos',
-    height: 1.6,
+    height: 5,
     type: ['electric','flying']
     },
   {
     name: 'Moltres',
-    height: 2,
+    height: 6,
     type: ['fire','flying']
   }
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height >= 6) {
+    document.write(`${(pokemonList[i].name)} is ${pokemonList[i].height}ft tall - Wowza! That is a BIG pokemon!<br/>`);
+  } else if (pokemonList[i].height > 3 && pokemonList[i].height <6) {
+    document.write(`${(pokemonList[i].name)} is ${pokemonList[i].height}ft tall - That is medium pokemon!<br/>`);
+  } else {
+    document.write(`${(pokemonList[i].name)} is ${pokemonList[i].height}ft tall - Aww! That pokemon is tiny!<br/>`);
+  }
+}
