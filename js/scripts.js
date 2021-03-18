da2c7d9f9ca9f502c95a87949e6f04ccf08fabac
fayerose87 <79806977+fayerose87@users.noreply.github.com>
2021-03-17 22:01:56 -0500

    Added comments

--- js/scripts.js
+++ js/scripts.js
@@ -16,11 +16,16 @@ let pokemonList = [
   }
 ];
 
+/* A for loop to iterate over each element of the array and then write it on the DOM.*/
+
 for (let i = 0; i < pokemonList.length; i++) {
+  //if height is greater than 6ft
   if (pokemonList[i].height >= 6) {
     document.write(`${(pokemonList[i].name)} is ${pokemonList[i].height}ft tall - Wowza! That is a BIG pokemon!<br/>`);
-  } else if (pokemonList[i].height > 3 && pokemonList[i].height <6) {
+    //if height is between 3ft and 6ft
+  } else if (pokemonList[i].height > 3 && pokemonList[i].height < 6) {
     document.write(`${(pokemonList[i].name)} is ${pokemonList[i].height}ft tall - Defintely not the tallest pokemon I've seen!<br/>`);
+    //if height is less than 6ft
   } else {
     document.write(`${(pokemonList[i].name)} is ${pokemonList[i].height}ft tall - Aww! That pokemon is so tiny!<br/>`);
   }
