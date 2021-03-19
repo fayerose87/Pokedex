4963bb2a7f4f74bb771576c3cd7dc56a0e60e8de
fayerose87 <79806977+fayerose87@users.noreply.github.com>
2021-03-18 21:51:15 -0500

    Added html for grid, image, and text styling

--- js/scripts.js
+++ js/scripts.js
@@ -2,31 +2,53 @@ let pokemonList = [
   {
     name: 'Bulbasaur',
     height: 2,
+    image: 'img/bulbasaur.png',
     type: ['grass','poison']
     },
   {
     name: 'Charizard',
     height: 5,
+    image: 'img/charizard.png',
     type: ['fire','flying']
     },
   {
     name: 'Lapras',
     height: 8,
+    image: 'img/lapras.png',
     type: ['water','ice']
+
   }
 ];
 
 /* A for loop to iterate over each element of the array and then write it on the DOM.*/
 
 for (let i = 0; i < pokemonList.length; i++) {
+  
   //if height is greater than 6ft
   if (pokemonList[i].height >= 6) {
-    document.write(`${(pokemonList[i].name)} is ${pokemonList[i].height}ft tall - Wowza! That is a BIG pokemon!<br/>`);
+    document.write(
+      `<div class="grid__item">
+      <img class"pokemon__pic" src="${(pokemonList[i].image)}">
+      <h2>${(pokemonList[i].name)}</h2> 
+      <p>${pokemonList[i].height}ft tall - Wowza! That is a BIG pokemon!</p>
+      </div>`);
+
     //if height is between 3ft and 6ft
   } else if (pokemonList[i].height > 3 && pokemonList[i].height < 6) {
-    document.write(`${(pokemonList[i].name)} is ${pokemonList[i].height}ft tall - Defintely not the tallest pokemon I've seen!<br/>`);
+    document.write(
+    `<div class="grid__item">
+    <img class"pokemon__pic" src="${(pokemonList[i].image)}">
+    <h2>${(pokemonList[i].name)}</h2>
+    <p>${pokemonList[i].height}ft tall - Defintely not the tallest pokemon I've seen!</p>
+    </div>`);
+
     //if height is less than 6ft
   } else {
-    document.write(`${(pokemonList[i].name)} is ${pokemonList[i].height}ft tall - Aww! That pokemon is so tiny!<br/>`);
+    document.write(
+    `<div class="grid__item">
+    <img class"pokemon__pic" src="${(pokemonList[i].image)}">
+    <h2>${(pokemonList[i].name)}</h2>
+    <p>${pokemonList[i].height}ft tall - Aww! That pokemon is so tiny!</p>
+    </div>`);
   }
 }
